bc64c7e66a18fb37cd1e76a2175103ceee50ee57
Christopher Stewart <cstewart90@users.noreply.github.com>
2016-10-22 14:15:25 +0100

    Check if current name is None.

--- update_player_names.py
+++ update_player_names.py
@@ -24,7 +24,8 @@ for key in r.sscan_iter('minqlx:players'):
     response = requests.get('{}&steamids=[{}]'.format(url, steam_id)).json()['response']
     name = response['players'][0]['personaname']
     names_key = 'minqlx:players:{}'.format(steam_id)
-    if r.lindex(names_key, 0).decode('UTF-8') != name:
+    current_name = r.lindex(names_key, 0)
+    if current_name is not None && current_name.decode('UTF-8') != name:
         r.lrem(names_key, 0, name)
         r.lpush(names_key, name)
     r.ltrim(names_key, 0, 19)
